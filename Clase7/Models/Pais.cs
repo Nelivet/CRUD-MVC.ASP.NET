2a51757c73c0cf159a04ad0dc9dd3538aab0b53c
Nelly Ivette Fernandez Rodriguez <ivettefernandezrodriguez@gmail.com>
2023-06-17 01:01:46 -0500

    Creacion clase Pais

---
+++ Clase7/Models/Pais.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Clase7.Models
+{
+    public class Pais
+    {
+        public string IdPais { get; set; }
+        public string NombrePais { get; set; }
+    }
+}
